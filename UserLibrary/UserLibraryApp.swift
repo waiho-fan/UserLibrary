ab0c13e11c27aae24255c9506e12a0f756914009
Gary Fan <tomfan419@gmail.com>
2025-01-08 07:00:15 +0800

    Initial Commit

---
+++ UserLibrary/UserLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserLibraryApp.swift
+//  UserLibrary
+//
+//  Created by Gary on 8/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct UserLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
